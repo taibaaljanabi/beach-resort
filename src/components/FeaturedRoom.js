66d1cd8b6b7f6b9d8b380c72fbac15c716b0a7fc
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-07-26 23:23:02 -0600

    commit

--- src/components/FeaturedRoom.js
+++ src/components/FeaturedRoom.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import {RoomContext} from '../Context'
+import Loading from './Loading'
+import Room from './Room'
+import Title from './Title'
 export default class FeaturedRoom extends Component {
     static contextType = RoomContext
   render() {
-     
-    
+     let { loading, featuredRooms: rooms }= this.context
+   rooms = rooms.map(room =>{
+       return <Room key={room.id} room={room}/>
+   })
 
     return (
-      <div>
-         from featured rooms 
+      <section className='featured-rooms'>
+      <Title title='الغرف المميزة'/>
+      <div className='featured-rooms-center'>
+      {loading? <Loading/>: rooms}
       </div>
+        ـ
+      </section>
     );
   }
 }
---
+++ src/components/Room.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import defaultImg from '../images/room-1.jpeg'
+
+export default function Room({room}) {
+   const {name, slug, images, price} = room
+  return (
+    <article className='room'>
+    <div className='img-container'>
+    <img src={images[0] || defaultImg} alt='single room'/>
+    <div className='price-top'>
+    <h6>${price}</h6>
+    <p>لكل ليلة</p>
+    </div>
+    <Link to ={`/rooms/${slug}`} className ='btn-primary room-link' >المميزات</Link>
+
+    </div>
+    </article>
+  );
+}
