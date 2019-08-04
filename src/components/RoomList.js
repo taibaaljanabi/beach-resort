502ccc5cda47e01d04a9c86420c7d9f48055f777
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-08-04 00:06:57 -0600

    commit

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Room from './Room'
 
-export default function RoomList() {
+export default function RoomList(rooms) {
+  if (rooms.length  === 0 ){
+    return(
+      <div className= 'empty-search'>
+      <h3>عذرا! ليس لدينا غرفة بهذه المواصفات</h3>
+      </div>
+    )
+  }
   return (
-    <div>
-      hello from RoomList
-    </div>
+    <section className='roomslist'>
+        <div className='roomlist-center'>
+      {rooms.map(item => {
+        return <Room key={item.id} room={item}/>
+      })}
+      </div>
+    </section>
   );
 }
+
--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -39,10 +39,10 @@ function RoomsContainer({context}){
     return <Loading/>
   }
   return (
-    <div>
+    <>
       <RoomsFilter rooms = {rooms}/>
       <RoomsList  rooms = {sortedRooms}/>
-    </div>
+    </>
   )
 }
 
