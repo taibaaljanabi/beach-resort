a753959a4c71d432ee00e096462431782a56a05b
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-08-03 23:13:26 -0600

    commit

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import RoomsFilter from './RoomFilter'
 import RoomsList from './RoomList'
-// import {RoomConsumer} from '../context'
 import Loading from './Loading'
+import {RoomConsumer} from '../Context'
 export default function RoomsContainer() {
   return (
-    <div>
-      Helo from rooms Container
-      <RoomsFilter/>
-      <RoomsList/>
-    </div>
+    <RoomConsumer>
+      {
+        (value) => {
+          console.log(value)
+          return (
+          <div>
+          Helo from rooms Container
+          <RoomsFilter/>
+          <RoomsList/>
+          </div>
+          )
+        }
+      }
+    </RoomConsumer>
+
   );
 }
