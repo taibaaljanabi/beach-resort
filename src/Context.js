6d410133dfd1014b50c58ebb0cf462c145ed970e
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-08-06 16:29:53 -0600

    commit

--- src/Context.js
+++ src/Context.js
@@ -58,7 +58,7 @@ getRoom =(slug)=>{
 }
 handleChange=(event)=>{
  const target = event.target
- const value = event.type === 'checkbox'?
+ const value = target.type === 'checkbox'?
  target.checked: target.value
   const name = event.target.name
  this.setState({
@@ -71,16 +71,56 @@ handleChange=(event)=>{
 }
 filterRooms=()=>{
   let {
-    rooms, type, capacity, price,minSize, maxSize, breakfast, pets
-  } = this.state
-
+      rooms,
+      type, 
+      capacity,
+      price,
+      minSize,
+      maxSize, 
+      breakfast,
+      pets
+      } = this.state
+  // all the rooms
   let tempRooms = [...rooms]
+  // transform values
+  capacity = parseInt(capacity )
+
+ // filter by type
   if(type !== 'all'){
     tempRooms = tempRooms.filter(room => room.type === type)
     }
-    
-  
+
+// filter by capacity
+if (capacity !== 1){
+  tempRooms = tempRooms.filter(room => room.capacity >= capacity)
 }
+//filter by price
+tempRooms = tempRooms.filter(room => room.price <= price)
+//filter by size
+tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+//filter by breakfast
+if(breakfast){
+  tempRooms = tempRooms.filter(room =>
+    room.breakfast === true
+)
+}
+
+
+// filter by pets
+if(pets){
+  tempRooms = tempRooms.filter(room =>
+    room.pets === true
+  )
+}
+
+// change state 
+    this.setState({
+      sortedRooms: tempRooms
+  
+    })
+}
+
 
 
 
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -21,6 +21,7 @@ export default function RoomFilter({rooms}) {
     maxPrice,
     minSize,
     maxSize ,
+    breakfast,
      pets
    } = context
    //get unique types
@@ -31,6 +32,9 @@ export default function RoomFilter({rooms}) {
    types = types.map((item, index )=>{
      return <option value = {item} key ={index} >{item}</option>
    })
+   let people = getUnique(rooms, 'capacity')
+   people = people.map((item, index) =>{
+     return <option key ={index} value = {item} >{item}</option>  })
 
   return <section className = 'filter-container'>
   <Title title = 'ابحث عن الغرف'/>
@@ -48,6 +52,75 @@ export default function RoomFilter({rooms}) {
   </select>
   </div>
   {/* end select type */}
+  {/* guests*/ }
+  <div className= 'form-group'>
+  <label htmlFor = 'capacity'>الضيوف</label>
+  <select name= 'capacity' 
+  id = 'capacity'
+   value = {capacity} 
+   className = 'form-control' 
+   onChange = {handleChange}>
+  {people}
+
+  </select>
+  </div>
+   {/* end guests*/}
+    {/*room price */}
+  <div className="form-group">
+  <label htmlFor='price'>سعر الغرفة ${price} </label>
+  <input type = 'range' name = 'price' min = {minPrice} max = {maxPrice} id = 'price' value = {price} onChange={handleChange} className = 'form-control'/>
+  
+  </div>
+
+    {/* end of room price */}
+     {/*  room size */}
+     <div className='form-group'>
+     <label htmlFor='size'>حجم الغرفة</label>
+     <div className= 'size-inputs'>
+     <input
+      type = 'number' 
+      name='minSize' 
+      id='size' 
+      value= {minSize} 
+      onChange = {handleChange}
+     className='size-input'
+     />
+     <input
+      type = 'number' 
+      name='maxSize' 
+      id='size' 
+      value= {maxSize} 
+      onChange = {handleChange}
+     className='size-input'
+     />
+
+     </div>
+     </div>
+      {/* end of room size */}
+       {/* extras */}
+       <div className='form-group'>
+       <div className='single-extra'>
+       <input type ='checkbox' name='breakfast' id = 'breakfast' checked = {breakfast}
+       onChange = {handleChange}/>
+       <lable 
+       htmlFor ='breakfast'>الفطور</lable>
+       </div>
+
+       <div className='single-extra'>
+       <input type ='checkbox' name='pets' id = 'pets' checked = {pets}
+       onChange = {handleChange}/>
+       <lable 
+       htmlFor ='pets'>الحيوانات الاليفة</lable>
+
+       </div>
+      
+       
+       
+       </div>
+       {/* end of extras */}
+
+
+
   </form>
   </section>
   
