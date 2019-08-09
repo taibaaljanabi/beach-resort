155460a672f77b7be0c1d85a2541d3044a4c9bcc
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-08-08 23:57:26 -0600

    commit

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-    space: '0o9igmp312xe',
-    accessToken:"4dBeMAa4D9h4voBtwCnZgcpazHK7NDFhvb-Wal51uFE"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_API_TOKEN
 })
